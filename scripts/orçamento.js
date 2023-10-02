49a1c303b81a4738a17c9c3fa7b2693a25bc7aaf
MiguelLeme <142456441+MiguelLeme@users.noreply.github.com>
2023-10-02 16:04:50 -0300

    Continuação da página de orçamento

--- scripts/orçamento.js
+++ scripts/orçamento.js
@@ -1,24 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
     var openBtn = document.getElementById("openBtn");
-    var closeBtn = document.getElementById("closeBtn");
     var sidebar = document.getElementById("mySidebar");
     var content = document.querySelector(".content");
 
     openBtn.addEventListener("click", function () {
         if (sidebar.style.width === "250px") {
             sidebar.style.width = "60px";
-            openBtn.innerHTML = "";
+           // openBtn.innerHTML = "";
             content.style.marginLeft = "60px"; // Redefine a margem quando a barra é fechada
+            openBtn.style.marginLeft = "0px";
+            openBtn.style.backgroundColor = "#2855e7";
+
+
         } else {
             sidebar.style.width = "250px";
-            openBtn.innerHTML = "";
+           // openBtn.innerHTML = "";
             content.style.marginLeft = "250px"; // Define a margem quando a barra é aberta
+            openBtn.style.marginLeft = "180px";
+            openBtn.style.backgroundColor = "#1a3591";
         }
     });
+});
 
-    closeBtn.addEventListener("click", function () {
-        sidebar.style.width = "60px";
-        openBtn.innerHTML = "Abrir";
-        content.style.marginLeft = "60px"; // Redefine a margem quando a barra é fechada
-    });
+// Obtém o elemento do ícone
+const icon = document.getElementById("searchIcon");
+
+// Adiciona um ouvinte de eventos para ativar a animação quando o mouse estiver sobre o ícone
+icon.addEventListener("mouseenter", () => {
+    icon.classList.add("fa-bounce");
+});
+
+// Remove a classe de animação quando o mouse deixar o ícone
+icon.addEventListener("mouseleave", () => {
+    icon.classList.remove("fa-bounce");
 });
